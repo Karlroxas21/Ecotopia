1f050cd8356a4568bdd9190895af37b02252c984
Karl Roxas <supercrane21@protonmail.com>
2023-04-21 21:45:30 +0800

    add scene for level2

---
+++ src/app/games/escape-room/scenes/level2/play-scene-level2.ts
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+
+export class PlayLevel2Scene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'play-level2-scene' });
+  }
+
+  config: Phaser.Types.Core.GameConfig | any;
+  init(data: { config: Phaser.Types.Core.GameConfig }) {
+    this.config = data.config;
+  }
+
+  create() {
+
+  }
+
+  override update() {
+
+
+  }
+
+
+}
