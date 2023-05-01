9e83b6de1f148c78cc41bde3d78df790a3726390
Vincent Regala <69969095+Vincent-star-hub@users.noreply.github.com>
2023-05-01 10:25:23 +0800

    homepage update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EscapeRoomComponent } from './games/escape-room/escape-room.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EscapeRoomComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
