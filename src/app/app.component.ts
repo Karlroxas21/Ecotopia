31b3d784501f2ad4ef7e34004a0c1d3e040b4310
Vincent Regala <69969095+Vincent-star-hub@users.noreply.github.com>
2023-05-18 19:46:01 +0800

    sass removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import SimpleParallax from 'simple-parallax-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ecotopia-capstone';
