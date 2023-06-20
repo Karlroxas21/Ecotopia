1910e66ae6316dbdbd703c5304da527451ca3814
Karl Roxas <supercrane21@protonmail.com>
2023-06-20 15:33:11 +0800

    change bg to white and add link to game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
