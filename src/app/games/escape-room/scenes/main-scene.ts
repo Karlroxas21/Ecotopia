0f65ef6584eb7bbc0eee1806a1d1879d1163eabe
Karl Roxas <supercrane21@protonmail.com>
2023-06-25 09:45:54 +0800

    add passengers and drivers in jeepneys

--- src/app/games/escape-room/scenes/main-scene.ts
+++ src/app/games/escape-room/scenes/main-scene.ts
@@ -204,8 +204,7 @@ export class MainScene extends Phaser.Scene {
 
     // Uncomment this when done in level 1!
     this.load.on('complete', () => {
-      //default-scene
-      this.scene.start('play-level2-scene', { config: this.game.config });
+      this.scene.start('default-scene', { config: this.game.config });
     });
 
     // Temporary to see Level 2 Scene Fast.
