4c07f8065e356e92ce46edc139314a6844f5126f
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2023-07-16 17:02:37 +0800

    add admin components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ import { AssessmentComponent } from './assessment/assessment.component';
 import { MaterialComponent } from './material/material.component';
 import { AdminCurrentissueComponent } from './admin-currentissue/admin-currentissue.component';
 // import { ADMINComponent } from './admin/admin.component';
-// import { AdminCasesComponent } from './admin-cases/admin-cases.component';
+import { AdminCasesComponent } from './admin-cases/admin-cases.component';
 // import { AdminInteractivevideoComponent } from './admin-interactivevideo/admin-interactivevideo.component';
 // import { AdminWebtoonComponent } from './admin-webtoon/admin-webtoon.component';
 // import { AdminSolutionsComponent } from './admin-solutions/admin-solutions.component';
@@ -53,8 +53,8 @@ const routes: Routes = [
   {component: AssessmentComponent, path: 'assessment'},
   // {component: ADMINComponent, path: 'ADMIN'},
   {component: MaterialComponent, path: 'material'},
-  {component: AdminCurrentissueComponent, path: 'admin-currentissue'}
-  // {component: AdminCasesComponent, path: 'admin-cases'},
+  {component: AdminCurrentissueComponent, path: 'admin-currentissue'},
+  {component: AdminCasesComponent, path: 'admin-cases'},
   // {component: AdminInteractivevideoComponent, path: 'admin-interactivevideo'},
   // {component: AdminWebtoonComponent, path: 'admin-webtoon'},
   // {component: AdminSolutionsComponent, path: 'admin-solutions'},
