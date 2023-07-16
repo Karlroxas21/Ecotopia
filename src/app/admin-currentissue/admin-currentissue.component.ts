7dcbbca9638d2487650a4d159939f6fd42b541fa
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2023-07-16 17:03:31 +0800

    add admin current issue

--- src/app/admin-currentissue/admin-currentissue.component.ts
+++ src/app/admin-currentissue/admin-currentissue.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-admin-currentissue',
   templateUrl: './admin-currentissue.component.html',
-  styleUrls: ['./admin-currentissue.component.css']
+  styleUrls: ['./admin-currentissue.component.css'],
 })
-export class AdminCurrentissueComponent {
 
+export class AdminCurrentissueComponent{
+  text: string = 'click to edit me';
+
+  @Input() value !: string;
+  editing: boolean = false;
+
+  startEditing() {
+    this.editing = true;
+  }
+
+  finishEditing(event: any) {
+    this.editing = false;
+    this.value = event.target.value;
+  }
 }
