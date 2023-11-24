4ebe9f961014f31429254cd20f04ef293ebdbde4
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2023-11-24 21:40:43 +0800

    convert all images to webp

--- src/app/games/escape-room/scenes/main-scene.ts
+++ src/app/games/escape-room/scenes/main-scene.ts
@@ -11,7 +11,7 @@ export class MainScene extends Phaser.Scene {
     // Background in default scene
     this.load.image(
       'default-scene-bg',
-      '../../../../assets/background/climate-escape-background.jpg'
+      '../../../../assets/background/climate-escape-background.webp'
     );
 
     // SFX
@@ -25,83 +25,83 @@ export class MainScene extends Phaser.Scene {
     this.load.audio('milestone', '../../../../assets/game/milestone-scene.mp3');
 
     // Cloud
-    this.load.image('cloud-1', '../../../../assets/game/scene1/cloud1.png');
-    this.load.image('cloud-2', '../../../../assets/game/scene1/cloud2.png');
-    this.load.image('cloud-3', '../../../../assets/game/scene1/cloud3.png');
-    this.load.image('cloud-4', '../../../../assets/game/scene1/cloud4.png');
-    this.load.image('cloud-5', '../../../../assets/game/scene1/cloud5.png');
-    this.load.image('cloud-6', '../../../../assets/game/scene1/cloud6.png');
-    this.load.image('cloud-7', '../../../../assets/game/scene1/cloud7.png');
-    this.load.image('cloud-8', '../../../../assets/game/scene1/cloud8.png');
-    this.load.image('cloud-9', '../../../../assets/game/scene1/cloud9.png');
-    this.load.image('cloud-0', '../../../../assets/game/scene1/cloud0.png');
+    this.load.image('cloud-1', '../../../../assets/game/scene1/cloud1.webp');
+    this.load.image('cloud-2', '../../../../assets/game/scene1/cloud2.webp');
+    this.load.image('cloud-3', '../../../../assets/game/scene1/cloud3.webp');
+    this.load.image('cloud-4', '../../../../assets/game/scene1/cloud4.webp');
+    this.load.image('cloud-5', '../../../../assets/game/scene1/cloud5.webp');
+    this.load.image('cloud-6', '../../../../assets/game/scene1/cloud6.webp');
+    this.load.image('cloud-7', '../../../../assets/game/scene1/cloud7.webp');
+    this.load.image('cloud-8', '../../../../assets/game/scene1/cloud8.webp');
+    this.load.image('cloud-9', '../../../../assets/game/scene1/cloud9.webp');
+    this.load.image('cloud-0', '../../../../assets/game/scene1/cloud0.webp');
    
     // Scene 1 Assets
-    this.load.image('scene1-bg', '../../../../assets/game/scene1/scene1-bg.png');
-    this.load.image('scene1-bg-correct', '../../../../assets/game/scene1/scene1-bg-correct.png');
-    this.load.image('scene1-bg-wrong', '../../../../assets/game/scene1/scene1-bg-wrong.png');
-    this.load.spritesheet('scene1-sprite', '../../../../assets/game/scene1/scene1-sprite.png',{
+    this.load.image('scene1-bg', '../../../../assets/game/scene1/scene1-bg.webp');
+    this.load.image('scene1-bg-correct', '../../../../assets/game/scene1/scene1-bg-correct.webp');
+    this.load.image('scene1-bg-wrong', '../../../../assets/game/scene1/scene1-bg-wrong.webp');
+    this.load.spritesheet('scene1-sprite', '../../../../assets/game/scene1/scene1-sprite.webp',{
       frameWidth: 800,
       frameHeight: 600
     })
 
     // Scene 2
-    this.load.image('scene2-bg', '../../../../assets/game/scene2/scene2-bg.png');
-    this.load.image('scene2-bg-correct', '../../../../assets/game/scene2/scene2-bg-correct.png');
-    this.load.image('scene2-bg-wrong', '../../../../assets/game/scene2/scene2-bg-wrong.png');
-    this.load.spritesheet('scene2-sprite', '../../../../assets/game/scene2/scene2-flow-sprite.png',{
+    this.load.image('scene2-bg', '../../../../assets/game/scene2/scene2-bg.webp');
+    this.load.image('scene2-bg-correct', '../../../../assets/game/scene2/scene2-bg-correct.webp');
+    this.load.image('scene2-bg-wrong', '../../../../assets/game/scene2/scene2-bg-wrong.webp');
+    this.load.spritesheet('scene2-sprite', '../../../../assets/game/scene2/scene2-flow-sprite.webp',{
       frameWidth: 800,
       frameHeight: 600
     })
 
     // Scene 3
-    this.load.image('scene3-bg', '../../../../assets/game/scene3/scene3-bg.png');
-    this.load.image('scene3-bg-correct', '../../../../assets/game/scene3/scene3-bg-correct.png');
-    this.load.image('scene3-bg-wrong', '../../../../assets/game/scene3/scene3-bg-wrong.png');
-    this.load.spritesheet('scene3-sprite', '../../../../assets/game/scene3/scene3-sprite.png',{
+    this.load.image('scene3-bg', '../../../../assets/game/scene3/scene3-bg.webp');
+    this.load.image('scene3-bg-correct', '../../../../assets/game/scene3/scene3-bg-correct.webp');
+    this.load.image('scene3-bg-wrong', '../../../../assets/game/scene3/scene3-bg-wrong.webp');
+    this.load.spritesheet('scene3-sprite', '../../../../assets/game/scene3/scene3-sprite.webp',{
       frameWidth: 800,
       frameHeight: 600
     })
 
     // Scene 4
-    this.load.image('scene4-bg', '../../../../assets/game/scene4/scene4-bg.png');
-    this.load.image('scene4-bg-wrong', '../../../../assets/game/scene4/scene4-bg-wrong.png');
-    this.load.image('scene4-bg-correct', '../../../../assets/game/scene4/scene4-bg-correct.png');
-    this.load.spritesheet('scene4-sprite', '../../../../assets/game/scene4/scene4-sprite.png',{
+    this.load.image('scene4-bg', '../../../../assets/game/scene4/scene4-bg.webp');
+    this.load.image('scene4-bg-wrong', '../../../../assets/game/scene4/scene4-bg-wrong.webp');
+    this.load.image('scene4-bg-correct', '../../../../assets/game/scene4/scene4-bg-correct.webp');
+    this.load.spritesheet('scene4-sprite', '../../../../assets/game/scene4/scene4-sprite.webp',{
       frameWidth: 800,
       frameHeight: 600
     })
 
     // Scene 5
-    this.load.image('scene5-bg', '../../../../assets/game/scene5/scene5-bg.png');
-    this.load.image('scene5-bg-wrong', '../../../../assets/game/scene5/scene5-bg-wrong.png');
-    this.load.image('scene5-bg-correct', '../../../../assets/game/scene5/scene5-bg-correct.png');
-    this.load.spritesheet('scene5-sprite', '../../../../assets/game/scene5/scene5-sprite.png',{
+    this.load.image('scene5-bg', '../../../../assets/game/scene5/scene5-bg.webp');
+    this.load.image('scene5-bg-wrong', '../../../../assets/game/scene5/scene5-bg-wrong.webp');
+    this.load.image('scene5-bg-correct', '../../../../assets/game/scene5/scene5-bg-correct.webp');
+    this.load.spritesheet('scene5-sprite', '../../../../assets/game/scene5/scene5-sprite.webp',{
       frameWidth: 800,
       frameHeight: 600
     })
     
     // Game Over Scene
-    this.load.image('game-over', '../../../../assets/game/game_over.png');
+    this.load.image('game-over', '../../../../assets/game/game_over.webp');
 
     // Milestone scene
-    this.load.spritesheet('congrats-bg', '../../../../assets/game/cngts_bg.png',{
+    this.load.spritesheet('congrats-bg', '../../../../assets/game/cngts_bg.webp',{
       frameWidth: 512,
       frameHeight: 598
     })
 
     // Game Over Scene
-    this.load.image('character-win', '../../../../assets/game/char_cngrts.png');
-    // this.load.image('congrats-bg', '../../../../assets/game/cngts_bg.png');
+    this.load.image('character-win', '../../../../assets/game/char_cngrts.webp');
+    // this.load.image('congrats-bg', '../../../../assets/game/cngts_bg.webp');
    
     // Example sprite character
-    this.load.spritesheet('character', '../../../../assets/game/chara.png',{
+    this.load.spritesheet('character', '../../../../assets/game/chara.webp',{
       frameWidth: 261,
       frameHeight: 233
     })
 
     // Heart sprite
-    this.load.spritesheet('heart-icon', '../../../../assets/game/heart_sprite.png', {
+    this.load.spritesheet('heart-icon', '../../../../assets/game/heart_sprite.webp', {
       frameWidth: 510,
       frameHeight: 510
     });
