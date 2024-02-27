e43108e70012ad5f0c277d48cfa6059689abe442
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2024-02-27 16:30:08 +0800

    Game made with unity

--- src/app/games/escape-room/escape-room.component.ts
+++ src/app/games/escape-room/escape-room.component.ts
@@ -1,32 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import Phaser from 'phaser';
-import { DefaultScene } from './scenes/default-scene';
-import { PrePlayScene } from './scenes/pre-play-scene';
-import { MainScene } from './scenes/main-scene';
-import { GameOver } from './scenes/game-over-scene';
-import { PlayScene } from './scenes/play-scene-level1';
-import { PlaySceneWrong } from './scenes/play-scene-wrong';
-import { PlaySceneCorrect } from './scenes/play-scene-correct';
-import { PlayScene2 } from './scenes/scene2/play-scene2';
-import { PlayScene2Wrong } from './scenes/scene2/play-scene2-wrong';
-import { PlayScene2Correct } from './scenes/scene2/play-scene2-correct';
-import { PlayScene3 } from './scenes/scene3/play-scene3';
-import { PlayScene3Correct } from './scenes/scene3/play-scene3-correct';
-import { PlayScene3Wrong } from './scenes/scene3/play-scene3-wrong';
-import { PlayScene4 } from './scenes/scene4/play-scene4';
-import { PlayScene4Correct } from './scenes/scene4/play-scene4-correct';
-import { PlayScene4Wrong } from './scenes/scene4/play-scene4-wrong';
-import { PlayScene5 } from './scenes/scene5/play-scene5';
-import { PlayScene5Correct } from './scenes/scene5/play-scene5-correct';
-import { PlayScene5Wrong } from './scenes/scene5/play-scene5-wrong';
-import { Milestone } from './scenes/milestone-scene';
-import { PrePlayScene2 } from './scenes/scene2/pre-play-scene2';
-import { PrePlayScene3 } from './scenes/scene3/pre-play-scene3';
-import { PrePlayScene4 } from './scenes/scene4/pre-play-scene4';
-import { PrePlayScene5 } from './scenes/scene5/pre-play-scene5';
-import { scoreService} from './scenes/score-service';
-import { heartPointsService } from './scenes/heart-service';
-import { Pre2PlayScene } from './scenes/pre2-play-scene'; // temporary
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -35,77 +8,22 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./escape-room.component.css']
 })
 export class EscapeRoomComponent implements OnInit{
-  phaserGame !: Phaser.Game ;
-  config: Phaser.Types.Core.GameConfig;
-  
+
+    ngOnInit() {
+        //@ts-ignore
+        createUnityInstance(document.querySelector("#unity-canvas"), {
+          dataUrl: "/assets/game/unity/Build/unity.data",
+          frameworkUrl: "/assets/game/unity/Build/unity.framework.js",
+          codeUrl: "/assets/game/unity/Build/unity.wasm",
+          streamingAssetsUrl: "StreamingAssets",
+          companyName: "Maafia",
+          productName: "Ecotopia",
+          productVersion: "1.0"
+        });
+  } 
   constructor(private titleService: Title){
-    this.config = {
-      type: Phaser.AUTO,
-      height: 600,
-      width: 800,
-      scene: [ MainScene, 
-        DefaultScene, 
-        PrePlayScene, 
-        GameOver,
-        PlayScene,
-        PlaySceneWrong,
-        PlaySceneCorrect,
-        PrePlayScene2,
-        Pre2PlayScene,
-        PlayScene2,
-        PlayScene2Wrong,
-        PlayScene2Correct,
-        PrePlayScene3,
-        PlayScene3,
-        PlayScene3Correct,
-        PlayScene3Wrong,
-        PrePlayScene4,
-        PlayScene4,
-        PlayScene4Correct,
-        PlayScene4Wrong,
-        PrePlayScene5,
-        PlayScene5,
-        PlayScene5Correct,
-        PlayScene5Wrong,
-        Milestone],
-      parent: 'gameContainer',
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: {y: 0}
-        }
-      }
-    }
-  }
-
-  createGame(){
-    this.phaserGame = new Phaser.Game(this.config);
-  }
-
-  getGameInstance(){
-    return this.phaserGame;
-  }
-
-  gameData: any;
-
-  ngOnInit() {
-    this.titleService.setTitle("Climate Escape");
     
-    if(!this.phaserGame){
-      this.createGame();
-    }else{
-      this.getGameInstance();
-    }
- }
-
-
- ngOnDestroy(){
-  if(this.phaserGame){
-    this.phaserGame.destroy(true);
   }
 
-  scoreService.resetScorePoints();
-  heartPointsService.resetHeartPoints();
- }
- 
+
 }
